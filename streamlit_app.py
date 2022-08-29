e4cbe10cb28739bd6907defcca8e57d34819a45b
yashwanth <112377152+yashwanth2001-oss@users.noreply.github.com>
2022-08-29 13:52:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 
-streamlit.title('Breakfast Menu)
+streamlit.title('Breakfast Menu')
 
 streamlit.text('Omega 3 & Blueberry Datmeal')
 streamlit.text('Kale , Spinach & Rocket Smoothie')
